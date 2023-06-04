4cacbcc207150f8a5a75aa7a8c498130c8b28658
Raul Luzetti <raul.chiodi@gmail.com>
2023-06-04 04:47:43 -0300

    update: readme

--- old/src/main/java/com/project/test/repositories/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.test.repositories;
-
-import com.project.test.entities.CompanyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {}
